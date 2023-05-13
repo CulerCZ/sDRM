92f3005d912ef9eb12a1ff644d7826565ceaa161
czhu <115071045+CulerCZ@users.noreply.github.com>
2023-05-13 16:51:20 +0800

    Create loadRawText.m
    load data from text file

---
+++ loadRawText.m
@@ -0,0 +1,9 @@
+% function to load raw scanning DRM dataset from .txt file
+function dataRaw = loadRawText()
+    
+    [filename, path] = uigetfile('*.txt', 'Select a text file');
+    dataFileName = fullfile(path, filename);
+    
+    dataRaw = load(dataFileName);
+    fprintf("Dataset load successfully from .txt file!\n");
+end
